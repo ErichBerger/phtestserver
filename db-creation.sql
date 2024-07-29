488517279b4fb18247ed20d7e4307e9eb75126ad
ErichBerger <erichcberger@gmail.com>
2024-07-28 21:48:39 -0500

    Improve db creation script

--- db-creation.sql
+++ db-creation.sql
@@ -4,36 +4,58 @@ create database phtestserver;
 
 use phtestserver;
 
-create table User (
-    id int auto_increment,
-    username varchar(20),
-    hashedPassword varchar(80),
-    fname varchar(20),
-    lname varchar(20),
-    primary key (id)
+
+CREATE TABLE `User` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(20) UNIQUE,
+  `hashedPassword` varchar(80),
+  `fname` varchar(20),
+  `lname` varchar(20),
+  `authLevel` int,
+  `npiNumber` int
 );
 
-create table Note (
-    id int auto_increment,
-    providerID int,
-    patient varchar(40),
-    service varchar(20),
-    startTime time,
-    endTime time,
-    summary text,
-    progress text,
-    response text,
-    assessmentStatus text,
-    riskFactors text,
-    emergencyInterventions text,
-    status tinytext,
-    serviceDate date,
-    primary key (id),
-    foreign key (providerID) references User(id)
+CREATE TABLE `Note` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `providerID` int,
+  `patientID` int,
+  `service` ENUM ('general', 'individual', 'family', 'group'),
+  `serviceDate` date,
+  `startTime` time,
+  `endTime` time,
+  `summary` text,
+  `status` tinytext
 );
 
-insert into User (username, hashedPassword, fname, lname) values ("test", "$2y$10$X8XV2SPQ4sVyYqCXpmTTlucH3QLqm7lStxkY4jjQQxuj5yV8WfMzm", "Bob", "Dobson");
-insert into User (username, hashedPassword, fname, lname) values ("test2", "$2y$10$X8XV2SPQ4sVyYqCXpmTTlucH3QLqm7lStxkY4jjQQxuj5yV8WfMzm", "Jenny", "Smith");
+CREATE TABLE `Patient` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `firstInitials` varchar(2),
+  `lastInitials` varchar(2)
+);
+
+CREATE TABLE `PatientDiagnosisCode` (
+  `patientID` int,
+  `code` varchar(15),
+  PRIMARY KEY (`patientID`, `code`)
+);
+
+CREATE TABLE `PatientGoal` (
+  `patientID` int,
+  `number` int,
+  `description` varchar(50),
+  PRIMARY KEY (`patientID`, `number`)
+);
+
+ALTER TABLE `Note` ADD FOREIGN KEY (`providerID`) REFERENCES `User` (`id`);
+
+ALTER TABLE `PatientGoal` ADD FOREIGN KEY (`patientID`) REFERENCES `Patient` (`id`);
+
+ALTER TABLE `Note` ADD FOREIGN KEY (`patientID`) REFERENCES `Patient` (`id`);
+
+ALTER TABLE `PatientDiagnosisCode` ADD FOREIGN KEY (`patientID`) REFERENCES `Patient` (`id`);
+
+insert into User (username, hashedPassword, fname, lname, authLevel) values ("test", "$2y$10$X8XV2SPQ4sVyYqCXpmTTlucH3QLqm7lStxkY4jjQQxuj5yV8WfMzm", "Bob", "Dobson", 1);
+insert into User (username, hashedPassword, fname, lname, authLevel) values ("test2", "$2y$10$X8XV2SPQ4sVyYqCXpmTTlucH3QLqm7lStxkY4jjQQxuj5yV8WfMzm", "Jenny", "Smith", 2);
 
 create user 'phadmin'@'localhost' identified by 'teambadass';
 
