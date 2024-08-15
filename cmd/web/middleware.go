0842aabc6dfef13fef09e866e80f2f42becaf938
Zohair <zohairkhanmail@gmail.com>
2024-08-15 15:08:27 -0500

    datefilter

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -21,7 +21,7 @@ func commonHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Set some common headers here
 		w.Header().Set("Server", "Go")
-		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
+		// w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
 		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
 		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "deny")
